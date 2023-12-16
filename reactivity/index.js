bc06566ddc3974ed47fdd537ea3a6e426875be06
houyuxin <houyuxin@mininglamp.com>
2023-12-16 22:03:44 +0800

    feat: [reactivity] effect function can have condition dep

--- reactivity/index.js
+++ reactivity/index.js
@@ -1,11 +1,25 @@
 const targetMap = new WeakMap();
 let activeEffect = null;
 
-const effect = (fn) => {
-  activeEffect = fn;
-  fn();
-  activeEffect = null;
-};
+function effect(fn) {
+  const effectFn = () => {
+    // fn中可能有条件分支，每次运行的时候都要重新收集一次依赖
+    cleanUp(effectFn);
+    activeEffect = effectFn;
+    // 实际运行有副作用的函数，收集该副作用的依赖
+    fn();
+    activeEffect = null;
+  };
+
+  effectFn.deps = [];
+
+  effectFn();
+}
+
+function cleanUp(effectFn) {
+  effectFn.deps.forEach((dep) => dep.delete(effectFn));
+  effectFn.deps.length = 0;
+}
 
 function track(target, key) {
   if (!activeEffect) return;
@@ -14,20 +28,23 @@ function track(target, key) {
   if (!depsMap) {
     targetMap.set(target, (depsMap = new Map()));
   }
-  let dep = depsMap.get(key);
-  if (!dep) {
-    depsMap.set(key, (dep = new Set()));
+  let deps = depsMap.get(key);
+  if (!deps) {
+    depsMap.set(key, (deps = new Set()));
   }
-  dep.add(activeEffect);
+  deps.add(activeEffect);
+  activeEffect.deps.push(deps);
 }
 
 function trigger(target, key) {
   let depsMap = targetMap.get(target);
   if (!depsMap) return;
-  let dep = depsMap.get(key);
-  if (dep) {
-    dep.forEach((effect) => effect());
-  }
+  let deps = depsMap.get(key);
+  // 运行effect函数会重新将所有的依赖解绑并重新绑定，可能会从set中清除并重新添加，可能会导致死循环
+  const effectsToRun = new Set(deps);
+  effectsToRun.forEach((effect) => {
+    effect();
+  });
 }
 
 const obj = new Proxy(
@@ -48,6 +65,6 @@ effect(() => {
   console.log(obj.ok ? obj.text : 'not');
 });
 
+obj.text = '123';
 obj.ok = false;
-obj.text = '12333';
-
+obj.text = '121231';
